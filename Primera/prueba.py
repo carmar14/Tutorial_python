56ef6bd9f0e6461b5384c6f5e3c67303b760e168
carmar14 <carmar141414@hotmail.com>
2022-07-11 18:32:34 -0500

    Prueba basica

---
+++ Primera/prueba.py
@@ -0,0 +1,10 @@
+a = 5
+b = 3.6
+c = a+b
+tipo = type(c)
+#----------con # comentamos lineas---------------------
+#----------con '''''' comentamos bloques de codigo------
+print("el contenido de la variable es: ", c)
+print("El tipo de la variable es:", tipo)
+
+
